e14460ee994a8ea01d8c852fc13e48096a98e429
yusufmuadz <yusufmuazd1@gmail.com>
2022-06-25 20:58:02 +0700

    update

---
+++ lib/beranda.dart
@@ -0,0 +1,17 @@
+// @dart=2.9
+
+import 'package:flutter/material.dart';
+
+class Beranda extends StatefulWidget {
+  @override
+  State<Beranda> createState() => _BerandaState();
+}
+
+class _BerandaState extends State<Beranda> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
